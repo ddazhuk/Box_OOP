6c00a5ddf3cffc8a6387a659c37cb7e6f7a7c5ba
ddazhuk <ddazhuk@gmail.com>
2018-03-03 00:15:33 +0200

    Initial commit

---
+++ src/Box.java
@@ -0,0 +1,11 @@
+public class Box {
+    double hight;
+    double weight;
+    double debth;
+    public void Vol(){
+        double Vol=hight*weight*debth;
+        System.out.println("Vol = "+Vol);
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Box one=new Box();
+        Box two=new Box();
+        one.debth=10;
+        one.hight=12;
+        one.weight=20;
+        two.debth=10;
+        two.hight=12;
+        two.weight=25;
+        one.Vol();
+        two.Vol();
+
+
+    }
+}
